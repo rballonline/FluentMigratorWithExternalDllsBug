357efc873fed5e6e4c6194dd795c8e82b0996de5
William Tilton <william.tilton@caci.com>
2020-09-23 11:21:09 -0700

    Bug

---
+++ TestMigration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.SqlClient;
+using FluentMigrator;
+
+namespace FluentMigratorWithExternalDlls
+{
+    [Migration(0)]
+    public class TestMigration : Migration
+    {
+        public override void Up()
+        {
+            using var connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            connection.Close();
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
